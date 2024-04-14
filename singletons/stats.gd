5dfab3dda2e92510139ddac7bf5046a4ad6692ca
partkyle <partkyle@gmail.com>
2024-04-14 16:30:33 -0700

    add floors cleared

--- singletons/stats.gd
+++ singletons/stats.gd
@@ -1,15 +1,20 @@
 extends Node
 
+var floors_cleared := 0
 var bees_shot := 0
 var bees_belled := 0
-
 var hives_destroyed := 0
 
 
 func _ready():
 	EventBus.game_started.connect(reset_stats)
+	EventBus.level_complete.connect(icnr_floors_cleared)
+
+func icnr_floors_cleared():
+	floors_cleared += 1
 
 func reset_stats():
+	floors_cleared = 0
 	bees_shot = 0
 	bees_belled = 0
 	hives_destroyed = 0
--- ui/stats_container.gd
+++ ui/stats_container.gd
@@ -1,10 +1,12 @@
 extends VBoxContainer
 
+@onready var floors_cleared = $FloorsCleared/Value
 @onready var bees_shot = $BeesShot/Value
 @onready var bees_belled = $BeesBelled/Value
 @onready var hives_destroyed = $HivesDestroyed/Value
 
 func update_stats():
+	floors_cleared.text = '%d' % Stats.floors_cleared
 	bees_shot.text = '%d' % Stats.bees_shot
 	bees_belled.text = '%d' % Stats.bees_belled
 	hives_destroyed.text = '%d' % Stats.hives_destroyed
